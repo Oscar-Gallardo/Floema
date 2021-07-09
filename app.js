4c82fd3d60320dae9f441767c2522dbff024e60b
Ocsar-Gallardo <gallardo_oscar@outlook.com>
2021-07-09 16:24:47 -0500

    Added the markup for the home page from the prismic api

--- app.js
+++ app.js
@@ -62,8 +62,22 @@ app.use((req, res, next) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const home = await api.getSingle('home')
+  const meta = await api.getSingle('meta')
+  const preloader = await api.getSingle('preloader')
+
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+    fetchLinks: 'product.image'
+  })
+
+  res.render('pages/home', {
+    collections,
+    home,
+    meta,
+    preloader
+  })
 })
 
 app.get('/about', async (req, res) => {
@@ -88,8 +102,6 @@ app.get('/collections', async (req, res) => {
   const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
   })
-
-  console.log(home)
 
   res.render('pages/collections', {
     collections,
