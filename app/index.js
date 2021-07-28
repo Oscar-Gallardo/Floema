589ff9f7a302842841988d228f8ca014d88cec58
oscar <gallardo_oscar@outlook.com>
2021-07-28 00:18:34 -0500

    Added class to animate the preloader, and added animations when the preloader animation ends and the page is loaded

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import each from 'lodash/each'
+import Preloader from 'components/Preloader'
 
 import About from 'pages/About'
 import Collections from 'pages/Collections'
@@ -7,9 +8,15 @@ import Home from 'pages/Home'
 
 class App {
   constructor () {
+    this.createPreloader()
     this.createContent()
     this.createPages()
     this.addLinkListeners()
+  }
+
+  createPreloader () {
+    this.preloader = new Preloader()
+    this.preloader.once('completed', this.onPreloaded.bind(this))
   }
 
   createContent () {
@@ -27,6 +34,10 @@ class App {
 
     this.page = this.pages[this.template]
     this.page.create()
+  }
+
+  onPreloaded () {
+    this.preloader.destroy()
     this.page.show()
   }
 
@@ -51,6 +62,8 @@ class App {
       this.page = this.pages[this.template]
       this.page.create()
       this.page.show()
+
+      this.addLinkListeners()
     } else {
       console.log(Error)
     }
